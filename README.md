bfeee4fcb533aff2600950534cd476727a3e7349
Manabu Niseki <manabu.niseki@gmail.com>
2019-12-11 20:44:38 +0900

    WIP

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# mobileconfig
-A collection of .mobileconfig in the wild
+# mobileconfig in the wild
+
+A collection of .mobileconfig in the wild.
+
+## Prerequisites
+
+- Ruby: `~> 2.6`
+- Bundler: `~> 2.0`
+
+## Installation
+
+```bash
+git clone https://github.com/ninoseki/mobileconfig.git
+cd mobileconfig
+bundle
+```
+
+## Usage
+
+### Crawl & collect .mobileconfig from URLs
+
+Create a plain text file which contains an IP address per line.
+
+```bash
+bundle exec ruby crawler.rb /path/to/file.txt
+```
+
+This script crawls IP addresses and collects .mobileconfig from the addresses.
+
+Collected .mobileconfig files are saved in `./samples`.
---
+++ scripts/collector.rb
@@ -0,0 +1,104 @@
+# frozen_string_literal: true
+
+require "down/http"
+require "fileutils"
+require "http"
+require "oga"
+require "openssl"
+
+class Collector
+  attr_reader :url
+
+  def initialize(url)
+    @url = normalize_url(url)
+  end
+
+  def mobileconfig?
+    mobileconfig_links.any?
+  end
+
+  def mobileconfig_links
+    links.select do |link|
+      link.end_with? ".mobileconfig"
+    end.map do |link|
+      to_url link
+    end
+  end
+
+  def download
+    FileUtils.mkdir_p base_dir
+    down = Down::Http.new(default_options)
+
+    mobileconfig_links.each do |link|
+      basename = File.basename(link)
+      begin
+        down.download(link, destination: download_to(basename))
+      rescue Down::Error => e
+        puts "Failed to download #{link} (#{e})"
+      end
+    end
+  end
+
+  private
+
+  def body
+    res = HTTP.timeout(3).get(url, default_options)
+    res.body.to_s
+  rescue HTTP::Error, OpenSSL::SSL::SSLError => _e
+    nil
+  end
+
+  def doc
+    return nil unless body
+
+    begin
+      @doc ||= Oga.parse_html(body)
+    rescue ArgumentError, LL::ParserError => _e
+      nil
+    end
+  end
+
+  def links
+    return [] if doc.nil?
+
+    doc.css("a").map do |a|
+      a.get("href")
+    end.compact
+  end
+
+  def to_url(href)
+    return href if href.start_with?("http://", "https://")
+
+    if href.start_with?("/")
+      url + href
+    else
+      "#{url}/#{href}"
+    end
+  end
+
+  def normalize_url(url)
+    url.end_with?("/") ? url[0..-2] : url
+  end
+
+  def host
+    URI(url).host
+  end
+
+  def base_dir
+    @base_dir ||= File.expand_path("./samples/#{host}/", __dir__)
+  end
+
+  def download_to(basename)
+    "#{base_dir}/#{basename}"
+  end
+
+  def ssl_context
+    ctx = OpenSSL::SSL::SSLContext.new
+    ctx.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    ctx
+  end
+
+  def default_options
+    @default_options ||= { ssl_context: ssl_context }
+  end
+end
